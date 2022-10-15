56bfe3b64fbccc9bbe16ae9f0652c439cbb9a5e3
= <=>
2022-10-15 00:14:16 -0300

    adicionar exercicio Fluxo da Engenharia

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Fluxograma da Engenharia");
+
+Console.Write("O Objeto está se movendo (S/N)? ");
+bool estaSeMovendo = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria? ");
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+String mensagem;
+
+if (!estaSeMovendo)
+{
+    if (!deveria)
+    {
+        mensagem = "Ótimo!";
+    }
+    else 
+    {
+        mensagem = "Use WD-40.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "Òtimo!";
+    }
+else
+{
+        mensagem = "Use Silver Tape.";
+}
+}
+Console.WriteLine();
+Console.WriteLine(mensagem);
+
